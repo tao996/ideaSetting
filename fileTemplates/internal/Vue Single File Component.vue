21fb974fc3389048d1cbeb2087586e4a86bee738
lvshutao <525191782@qq.com>
2020-07-20 08:03:26 +0800

    IU-2020.1.3 <shutao@MacBook-Pro.local Create markdown.xml, my-PhpTest.xml, Vue Single File Component.vue, project.default.xml, Default.xml, find.xml, vgo.xml, _@user_Darcula.icls, Zen HTML.xml, path.macros.xml, mavenVersion.xml, React.xml, Default.xml, RESTful Web Services.xml, JavaScript.xml, AngularJS.xml, Kotlin.xml, Zen XSL.xml, plain.xml, surround.xml, workspace.xml, my-Layui.xml, editor.codeinsight.xml, keymap.xml, iterations.xml, editor.rich.copy.xml, editor.xml, .DS_Store, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, my-JavaScript.xml, web-types-registry.xml, my-flutter.xml, HTML_XML.xml, Zen CSS.xml, filetypes.xml, php.xml, projectView.xml, Go.xml, xsl.xml, plugin_ui.xml, JavaScript Testing.xml, colors.scheme.xml, my-Yii2.xml, my-nz.xml, databaseDrivers.xml, templates.xml, Default for macOS copy.xml, Go-my.xml, Groovy.xml, Shell Script.xml, output.xml, my-vue.xml, go.sdk.xml, Angular.xml, jdk.table.xml, laf.xml, my-Angular.xml, my-yii2ext.xml, HTTP Request.xml, my-Php.xml, vcs.xml, grazie_global.xml, Go-fsl.xml, my-moment.xml, Go Struct Tags.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+#[[$END$]]#
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    
+    export default {
+        name:'${NAME}',
+        data () {
+            return {}
+        },
+        props: {},
+        computed: {},
+        methods: {},
+        mounted () {
+            console.log('mounted ~~${NAME}~~')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
